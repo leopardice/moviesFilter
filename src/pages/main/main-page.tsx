984a62bee6a6ce32ffb4a1b093370b6dc1b06f89
Igor Panov <igor.panov.hse@gmail.com>
2022-07-26 17:16:05 +0300

    filter button

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { Container, Grid } from '@mui/material';
-import FiltersMenu from './components/FiltersMenu/FiltersMenu';
-import MoviesList from './components/MoviesList/MoviesList';
+import React, { useState } from "react";
+import { Container, Grid, useMediaQuery, useTheme } from "@mui/material";
+import FiltersMenu from "./components/FiltersMenu/FiltersMenu";
+import MoviesList from "./components/MoviesList/MoviesList";
+import FilterButton from "./components/FiltersMenu/components/show-filters-button";
 
-const MainPage = () => (
-  <Container>
-    <Grid container spacing={1}>
-      <FiltersMenu />
-      <MoviesList />
-    </Grid>
-  </Container>
-);
+const MainPage = () => {
+  const theme = useTheme();
+  const phone = useMediaQuery(theme.breakpoints.only("xs"));
+
+  const gridDirection = phone ? "column" : "row";
+
+  const [filterModal, setFilterModal] = useState(false);
+
+  const filterButtonHandler = () => {
+    setFilterModal((prevState) => !prevState);
+  };
+
+  return (
+    <Container>
+      <Grid container spacing={1} direction={gridDirection}>
+        {!phone && <FiltersMenu />}
+        {filterModal && <FiltersMenu />}
+        {phone && (
+          <FilterButton onClick={filterButtonHandler} isOpen={filterModal} />
+        )}
+        <MoviesList />
+      </Grid>
+    </Container>
+  );
+};
 
 export default MainPage;
